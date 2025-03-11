964a93acc5bf388b6fe19f9e99e1466647c79da3
jerecede <jeremias10458@gmail.com>
2025-03-11 13:30:48 +0100

    creazione struttura html css

---
+++ src/app.js
@@ -0,0 +1,81 @@
+import DataService from "./services/data-service.js";
+
+const service = new DataService();
+
+const studentData = service.getStudentsData();
+
+// //CREA DIV
+
+// const container = document.getElementById('students-container');
+
+// for (let i = 0; i < studentData.length; i++) {
+//     const student = studentData[i];
+    
+//     //CREA DIV STUDENTE E ASSEGNA CLASSE
+
+//     const studentContainer = document.createElement('div');
+//     studentContainer.classList.add('student-container');
+
+//     //CREA H3 name DA METTERE DENTRO DIV
+
+//     const nameContainer = document.createElement('h3');
+//     // nameContainer.style.color = 'red';
+//     const nameNode = document.createTextNode(student.name + ' ' + student.surname); //aggiunge text
+//     nameContainer.appendChild(nameNode);
+//     studentContainer.appendChild(nameContainer);
+
+//     //CREA SPAN nationality DA METTERE DENTRO DIV
+
+//     const countryContainer = document.createElement('span');
+//     const countryNode = document.createTextNode('Nazionalità: ' + student.nationality); //aggiunge text
+//     countryContainer.appendChild(countryNode);
+//     // const countryContainer = document.createElement('span');
+//     // countryContainer.innerText = 'Nazionalità: ' + student.nationality; //innerText innerHTML textContent
+//     studentContainer.appendChild(countryContainer);
+//     container.appendChild(studentContainer);
+
+// }
+
+const container = document.getElementById('students-container');
+
+for (let i = 0; i < studentData.length; i++) {
+    const student = studentData[i];
+
+    const studentContainer = document.createElement('div');
+    if(i%2 === 0){
+        studentContainer.classList.add('student-container');
+    } else {
+        studentContainer.classList.add('student-container2');
+    }
+
+    const nameContainer = document.createElement('span');
+    nameContainer.classList.add('name-container');
+    const nameNode = document.createTextNode(student.name + ' ' + student.surname);
+    nameContainer.appendChild(nameNode);
+
+    const countryContainer = document.createElement('span');
+    countryContainer.classList.add('country-container');
+    const countryNode = document.createTextNode('Nazionalità: ' + student.nationality);
+    countryContainer.appendChild(countryNode);
+
+    const genderContainer = document.createElement('span');
+    genderContainer.classList.add('gender-container');
+    const genderNode = document.createTextNode('Gender: ' + student.gender);
+    genderContainer.appendChild(genderNode);
+
+    const now = new Date();
+    const year = now.getFullYear();
+    const age = year - student.yob;
+    const ageContainer = document.createElement('span');
+    ageContainer.classList.add('age-container');
+    const ageNode = document.createTextNode('Age: ' + age);
+    ageContainer.appendChild(ageNode);
+
+    studentContainer.appendChild(nameContainer);
+    studentContainer.appendChild(countryContainer);
+    studentContainer.appendChild(genderContainer);
+    studentContainer.appendChild(ageContainer);
+
+    container.appendChild(studentContainer);
+}
+
